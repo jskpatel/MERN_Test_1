143dfe3afd7cfb326712604ac880b8142e5183ce
unknown <developersunilpatel@gmail.com>
2023-01-02 00:43:05 +0530

    Create new user

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017";
+const mongoURI = "mongodb://localhost:27017/mern_test_1_inotebook";
 
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,43 @@
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  const user = User(req.body);
-  user.save();
-  res.send(req.body);
-});
+router.post(
+  "/createUser",
+  [
+    body("name", "Enter valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be atleast 5 character").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists" });
+      }
+
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+
+      res.json(user);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
 
 module.exports = router;
